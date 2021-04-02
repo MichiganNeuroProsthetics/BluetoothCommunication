1314d35412234833f5c7d62d82e280f0b791e468
Earth <earthpat@umich.edu>
2021-04-02 18:44:03 -0400

    added ability to communicate with BLE module

--- iOS_app/BLEApp/blue/ViewController.swift
+++ iOS_app/BLEApp/blue/ViewController.swift
@@ -12,11 +12,40 @@ import CoreBluetooth
 var successful = false
 var myPeripheral: CBPeripheral!
 
+struct CBUUIDs{
+    static let kBLEService_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+
+    static let kBLE_Characteristic_uuid_Tx = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+
+    static let kBLE_Characteristic_uuid_Rx = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+
+
+
+    static let BLEService_UUID = CBUUID(string: kBLEService_UUID)
+
+    static let BLE_Characteristic_uuid_Tx = CBUUID(string: kBLE_Characteristic_uuid_Tx)//(Property = Write without response)
+
+    static let BLE_Characteristic_uuid_Rx = CBUUID(string: kBLE_Characteristic_uuid_Rx)// (Property = Read/Notify)
+
+    static let BLE_char = CBUUID(string: "FFE1")
+
+
+}
+
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     var centralManager: CBCentralManager!
-
-    @IBOutlet weak var Switch: UISwitch!
     
+    private var txCharacteristic: CBCharacteristic!
+    private var rxCharacteristic: CBCharacteristic!
+
+    @IBOutlet weak var sendHelloWorld: UIButton!
+    
+    @IBAction func sendHelloWorld(_ sender: Any) {
+        writeOutgoingValue(data: "Hello World")
+    }
+    
+    
+    @IBOutlet weak var Switch: UISwitch!
     @IBAction func Switch(_ sender: UISwitch){
         if sender.isOn == false{
             if successful == true{
@@ -80,10 +109,76 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         myPeripheral.discoverServices(nil)
     }
     
-    func peripheral(_ peripheral: CBPeripheral,
-           didUpdateValueFor characteristic: CBCharacteristic,
-           error: Error?){}
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print("*******************************************************")
+        if ((error) != nil) {
+            print("Error discovering services: \(error!.localizedDescription)")
+            return
+        }
+
+        guard let services = peripheral.services else {
+            return
+        }
+
+        //We need to discover the all characteristic
+
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+        print("Discovered Services: \(services)")
+
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+     
+          var characteristicASCIIValue = NSString()
+     
+          guard characteristic == rxCharacteristic,
+     
+          let characteristicValue = characteristic.value,
+          let ASCIIstring = NSString(data: characteristicValue, encoding: String.Encoding.utf8.rawValue) else { return }
+     
+          characteristicASCIIValue = ASCIIstring
+     
+          print("Value Recieved: \((characteristicASCIIValue as String))")
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else {
+                return
+        }
+        
+        print("Found \(characteristics.count) characteristics.")
+
+        for characteristic in characteristics {
+            print(characteristic.uuid)
+            if characteristic.uuid.isEqual(CBUUIDs.BLE_char)  {
+                rxCharacteristic = characteristic
+                peripheral.setNotifyValue(true, for: rxCharacteristic!)
+                peripheral.readValue(for: characteristic)
+                print("RX Characteristic: \(rxCharacteristic.uuid)")
+            }
+            if characteristic.uuid.isEqual(CBUUIDs.BLE_char){
+                txCharacteristic = characteristic
+                print("TX Characteristic: \(txCharacteristic.uuid)")
+            }
+
+        }
+    }
     
+    
+    func writeOutgoingValue(data: String){
+          
+        let valueString = (data as NSString).data(using: String.Encoding.utf8.rawValue)
+        
+        if let peripheral = myPeripheral {
+              
+          if let txCharacteristic = txCharacteristic {
+                  
+            peripheral.writeValue(valueString!, for: txCharacteristic, type: CBCharacteristicWriteType.withoutResponse)
+              }
+          }
+      }
     
     
     override func viewDidLoad() {
